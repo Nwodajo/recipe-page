5c5b1813179205fe3f8e560f661280f2a8603446
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2025-10-22 23:42:15 -0700

    Add js.js files for JavaScript setup

---
+++ js.js
@@ -0,0 +1,80 @@
+Script started on Wed Oct 22 23:12:19 2025
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;32m➜  [36mrecipe-page[00m [K[?1h=[?2004h
+
+[0m[27m[24m[J[01;32m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [Kggit rev-list--           g  ggit rec-list --count HEAD[?1l>[?2004l
+
+]2;git rec-list --count HEAD]1;gitgit: 'rec-list' is not a git command. See 'git --help'.
+
+The most similar command is
+	rev-list
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004h[7mrecipe-page git:(phase-3-js)[27m[28D[27mr[27me[27mc[27mi[27mp[27me[27m-[27mp[27ma[27mg[27me[27m [27mg[27mi[27mt[27m:[27m([27mp[27mh[27ma[27ms[27me[27m-[27m3[27m-[27mj[27ms[27m)[?1l>[?2004l
+
+]2;recipe-page git:(phase-3-js)]1;recipe-pagezsh: unknown file attribute: h
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit rec-list --count HEAD[?1l>[?2004l
+
+]2;git rec-list --count HEAD]1;gitgit: 'rec-list' is not a git command. See 'git --help'.
+
+The most similar command is
+	rev-list
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit rev-list --count HEAD[?1l>[?2004l
+
+]2;git rev-list --count HEAD]1;git7
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;32m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004h[7mrecipe-page git:(phase-3-js)[27m[28D[27mr[27me[27mc[27mi[27mp[27me[27m-[27mp[27ma[27mg[27me[27m [27mg[27mi[27mt[27m:[27m([27mp[27mh[27ma[27ms[27me[27m-[27m3[27m-[27mj[27ms[27m)[?1l>[?2004l
+
+]2;recipe-page git:(phase-3-js)]1;recipe-pagezsh: unknown file attribute: h
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit add script.js[?1l>[?2004l
+
+]2;git add script.js]1;gitfatal: pathspec 'script.js' did not match any files
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit con mmit -m "Creat te script.js file for ph 
+[Ka
+ase 3"[?1l>[?2004l
+
+]2;git commit -m "Create script.js file for phase 3"]1;gitOn branch phase-3-js
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	[31m.js[m
+
+nothing added to commit but untracked files present (use "git add" to track)
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit rev-list --count Head   EAD[?1l>[?2004l
+
+]2;git rev-list --count HEAD]1;git7
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;32m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit add js.js[?1l>[?2004l
+
+]2;git add js.js]1;gitfatal: pathspec 'js.js' did not match any files
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004hggit add script.js[?1l>[?2004l
+
+]2;git add script.js]1;gitfatal: pathspec 'script.js' did not match any files
+[1m[7m%[27m[1m[0m                                                                            
+ 
+]2;tpl925_7@MacBookPro:~/Desktop/tecthtonica-project/recipe-page]1;..t/recipe-page]7;file://MacBookPro.attlocal.net/Users/tpl925_7/Desktop/tecthtonica-project/recipe-page\
+[0m[27m[24m[J[01;31m➜  [36mrecipe-page[00m [01;34mgit:([31mphase-3-js[34m) [33m✗[00m [K[?1h=[?2004h
