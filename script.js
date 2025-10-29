8307bb8304c1bb870792874999be394b952f5d0e
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2025-10-29 00:09:08 -0700

    Adding 10 comments and fixed title color function

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Add a new ingredient automatically 
 const ingredientList = document.querySelector('#ingredients ul');
-// create a new Li
+// create a new <li> element
 const li = document.createElement('li');
-// Set the text
+// Set the text of the new list item
 li.textContent =' 1 tsp black pepper';
-// give it the same Bootstrap style 
+// Add a Bootstrap style to the list item 
 li.classList.add('list-group-item');
-// attach it to the list
+// attach the new item to the ingredient list 
 ingredientList.appendChild(li);
 
 // Add click to strike feauture
 
 const items = document.querySelectorAll('#ingredients li');
 items.forEach((item) => {
+    // Toggle "checked" class when an item is clicked
     item.addEventListener('click',() => {
         item.classList.toggle('checked');
     });
 
 });
-// Change the Recipe title color
+// Add click even to change  title color
 const colorButton = document.querySelector('#color-btn');
+// Select the recipe title 
 const recipeTitle = document.querySelector('#recipe-title');
-
+// Change the title color when button is clicked
 colorButton.addEventListener('click',()=>{
-    recipeTitle.computedStyleMap.color ='orange';
+    recipeTitle.style.color ='orange';
 });
